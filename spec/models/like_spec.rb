9a489498ec9a109dff55672280c6ae656a0ac953
Paul Onchera <hesbonpoisson@gmail.com>
2022-11-25 17:15:35 +0300

    Fix linter errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  user1 = User.create(name: "Paul", photo: "photo", bio: "bio", posts_counter: 0)
-  post1 = Post.create(title: "title1", text: "text1", user: user1)
+  user1 = User.create(name: 'Paul', photo: 'photo', bio: 'bio', posts_counter: 0)
+  post1 = Post.create(title: 'title1', text: 'text1', user: user1)
   Like.create(user: user1, post: post1)
 
   it '#counter' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Spec, type: :model do
-  subject { User.new(name: "Paul", photo: "photo", bio: "bio", posts_counter: 0) }
+  subject { User.new(name: 'Paul', photo: 'photo', bio: 'bio', posts_counter: 0) }
   before { subject.save }
 
-  it "name should be present" do
+  it 'name should be present' do
     subject.name = nil
     expect(subject).to_not be_valid
   end
 
-  it "posts counter should be valid" do
+  it 'posts counter should be valid' do
     subject.posts_counter = nil
     expect(subject).to_not be_valid
   end
 
-  it "returns the three recent posts for a user" do
+  it 'returns the three recent posts for a user' do
     subject.posts_counter = 0
     Post.create(user: subject, title: 'title1', text: 'text1')
     Post.create(user: subject, title: 'title2', text: 'text2')
