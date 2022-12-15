602b3fbaf5199b084c7628068efed1750e577b16
Riya Bulia <riyabulia12@gmail.com>
2022-12-15 21:06:26 +0530

    Add authentication in the controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,1 +1,17 @@
+require '././app/commands/authenticate_user'
 
+class Api::V1::AuthenticationController < Api::V1::ApplicationController
+  skip_before_action :authenticate_request
+
+  def authenticate
+    @user = User.find_by_email(params[:email])
+    if @user&.valid_password?(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     username: @user.name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+end
